aae9e0bef006514553ecadf1bb900b0709c58cae
Byron Lathi <byron.lathi@student.lovett.org>
2016-11-22 23:43:44 -0500

    Added Constants
    This contains some constant values, such as button numbers and joystick
    ports, as well as max speed values and such.

---
+++ First Memeworks/src/org/usfirst/frc/team5482/robot/Constants.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team5482.robot;
+
+public class Constants {
+	public static final int STICK_PORT = 0;
+	public static final int TRIGGER_NUMBER = 1;
+	public static final int THUMB_BUTTON_NUMBER = 5;
+}
