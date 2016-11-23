f2d6906290ae4b0f3a684a157dba1a3214bea4d1
Byron Lathi <byron.lathi@student.lovett.org>
2016-11-23 00:06:11 -0500

    Added javadoc and max speeds to Constants.

--- First Memeworks/src/org/usfirst/frc/team5482/robot/Constants.java
+++ First Memeworks/src/org/usfirst/frc/team5482/robot/Constants.java
@@ -1,7 +1,30 @@
 package org.usfirst.frc.team5482.robot;
 
+/**
+ * The Constants class is used for holding any constants that do not relate to
+ * the physical aspects of the robot. Those should all be directed towards
+ * RobotMap. Things that should go here are things like
+ * <ul>
+ * <li>Joystick Ports</li>
+ * <li>Max Speeds</li>
+ * <li>Coefficients for equations</li>
+ * </ul>
+ * 
+ * <p>
+ * 
+ * @author Byron Lathi
+ *
+ */
 public class Constants {
+	
+	// Joystick Ports
 	public static final int STICK_PORT = 0;
+	
+	// Joystick Button Mapping
 	public static final int TRIGGER_NUMBER = 1;
 	public static final int THUMB_BUTTON_NUMBER = 5;
+	
+	// Max Speeds
+	public static final double GLOBAL_MAX_MOVE_SPEED = 0.9;
+	public static final double GLOBAL_MAX_MANIP_SPEED = 0.9;
 }
